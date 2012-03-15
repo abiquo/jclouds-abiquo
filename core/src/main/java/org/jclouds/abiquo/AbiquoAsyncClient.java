9f02a7dec5d556f9eb35ce9209aa7ff9c0c65318
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-03-15 10:23:35 +0100

    Upgraded to version 2.0

--- core/src/main/java/org/jclouds/abiquo/AbiquoAsyncClient.java
+++ core/src/main/java/org/jclouds/abiquo/AbiquoAsyncClient.java
@@ -47,7 +47,7 @@ public interface AbiquoAsyncClient
     /**
      * The supported build version of the Abiquo Api.
      */
-    public static final String BUILD_VERSION = "2908474-8e9e025";
+    public static final String BUILD_VERSION = "7bbfe95-158721b";
 
     /**
      * Provides asynchronous access to Admin features.
