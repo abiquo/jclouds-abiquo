072405b665fe598970bc959769ace60ed7645771
Albert Puig <apuig@abiquo.com>
2012-04-17 15:03:22 +0200

    missing newline after license

--- core/src/main/java/org/jclouds/abiquo/functions/AppendApiVersionToAbiquoMimeType.java
+++ core/src/main/java/org/jclouds/abiquo/functions/AppendApiVersionToAbiquoMimeType.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.jclouds.abiquo.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
--- core/src/test/java/org/jclouds/abiquo/rest/internal/AbiquoHttpAsyncClientTest.java
+++ core/src/test/java/org/jclouds/abiquo/rest/internal/AbiquoHttpAsyncClientTest.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.jclouds.abiquo.rest.internal;
 
 import java.io.IOException;
