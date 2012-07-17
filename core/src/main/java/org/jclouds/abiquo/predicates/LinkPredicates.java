9b247700ecfd9b2c97a2c03ca82460648b8c17cb
Albert Puig <apuig@abiquo.com>
2012-07-17 18:16:24 +0200

    @nacx doen't like new lines after licese :P

--- core/src/main/java/org/jclouds/abiquo/predicates/LinkPredicates.java
+++ core/src/main/java/org/jclouds/abiquo/predicates/LinkPredicates.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.jclouds.abiquo.predicates;
 
 import static com.google.common.base.Preconditions.checkNotNull;
